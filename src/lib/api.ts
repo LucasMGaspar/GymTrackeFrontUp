98e6188f2294e02221a385b57586037c868a552e
Lucas Gaspar <50829518+LucasMGaspar@users.noreply.github.com>
2025-07-22 11:34:38 -0300

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://jcrw8tg1-3000.brs.devtunnels.ms',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
