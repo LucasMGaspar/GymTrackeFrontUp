6917f8bd5ebfed265bba3095c52c195b674a5780
Lucas Gaspar <50829518+LucasMGaspar@users.noreply.github.com>
2025-07-22 15:37:23 -0300

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://jcrw8tg1-3000.brs.devtunnels.ms',
+  baseURL: 'gymtrackebackfinal-production-7b40.up.railway.app',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
